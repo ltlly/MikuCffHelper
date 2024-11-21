5b55c12b962a3bb9b9216babc795db91e35f7ff3
ltlly <a1253213025@163.com>
2024-11-21 21:54:32 +0800

    fix my_copy_expr

--- passes/spiltIfPass.py
+++ passes/spiltIfPass.py
@@ -6,6 +6,23 @@ from binaryninja import *
 from ..utils import get_basic_block_at
 
 
+def my_copy_expr(llil: LowLevelILFunction, instr: LowLevelILInstruction):
+    # api: LowLevelILFunction.copy_expr
+    # def copy_expr(self, original: LowLevelILInstruction) -> ExpressionIndex:
+    #     """
+    #     ``copy_expr`` adds an expression to the function which is equivalent to the given expression
+    #
+    #     :param LowLevelILInstruction original: the original IL Instruction you want to copy
+    #     :return: The index of the newly copied expression
+    #     """
+    #     return self.expr(original.operation, original.raw_operands[0], original.raw_operands[1],
+    #                      original.raw_operands[2], original.raw_operands[3], original.size, original.flags)
+
+    flags = instr.flags if instr.flags != "" else None
+    return llil.expr(instr.operation, instr.raw_operands[0], instr.raw_operands[1], instr.raw_operands[2],
+                     instr.raw_operands[3], instr.size, flags)
+
+
 def pass_spilt_if_block(llil: LowLevelILFunction):
     bbs = list(llil.basic_blocks)
     def traverse_if_bb(instr):
@@ -24,9 +41,7 @@ def pass_spilt_if_block(llil: LowLevelILFunction):
         log_info(f"copying [::] {ifinstr}")
         goto_label = LowLevelILLabel()
         llil.mark_label(goto_label)
-
-        #copy expr 的api设计有问题
-        llil.append(llil.copy_expr(ifinstr))
+        llil.append(my_copy_expr(llil, ifinstr))
         llil.replace_expr(ifinstr, llil.goto(goto_label))
         updated = True
     return updated
